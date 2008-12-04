4b8daefe1758aa8c25eecc5e149f2f01341e52a2
Luis Gustavo S. Barreto <gustavo@ossystems.com.br>
2008-12-04 09:55:36 -0200

    Fixes warnings

--- main.c
+++ main.c
@@ -16,6 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
+#include <string.h>
+
 #include <gdk/gdkkeysyms.h>
 #include <gtk/gtk.h>
 #include <vte/vte.h>
